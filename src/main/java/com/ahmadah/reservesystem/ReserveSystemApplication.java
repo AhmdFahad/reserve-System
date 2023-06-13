4a96aeab82daf3fd5594630dac3227f6cf45b657
Ahmad Fahad <ahmd66699@gmail.com>
2023-06-13 22:38:15 +0300

    init project

---
+++ src/main/java/com/ahmadah/reservesystem/ReserveSystemApplication.java
@@ -0,0 +1,12 @@
+package com.ahmadah.reservesystem;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveSystemApplication.class, args);
+    }
+
+}
